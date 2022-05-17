2d757bdcff256c554c2e61f9d995bc512ef7e597
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-17 14:46:33 +0530

    Added: Second Case to check and validate user's last name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -19,4 +19,20 @@ public class UserRegistration {
             System.out.println("First name is Invalid.");
         return r;
     }
+
+    public static void validLastName() {
+
+        System.out.println("Enter last name: ");
+        String lName = sc.next();
+        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
+
+        Pattern pattern1 = Pattern.compile(regex1);
+        Matcher matcher1 = pattern1.matcher(lName);
+        boolean r = matcher1.matches();
+
+        if (r)
+            System.out.println("It is a valid last name.");
+        else
+            System.out.println("It is invalid last name.");
+    }
 }
--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -4,11 +4,16 @@ public class UserRegistrationMain {
     public static void main(String[] args) {
         System.out.println("****Welcome to the User Registration Program****");
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter 1 : to validate First Name ");
+        System.out.println("Enter 2 : to validate Last Name ");
 
         switch (scanner.nextInt()) {
             case 1:
                 UserRegistration.validFirstName();
+                break;
+            case 2:
+                UserRegistration.validLastName();
                 break;
             default:
                 System.out.println("Enter valid input.");
